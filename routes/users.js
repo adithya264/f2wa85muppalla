74c7981d907b1fb6ede19c8d21c27bc02d338c38
adithya264 <adithya369varaprasad@gmail.com>
2022-10-26 18:09:07 -0500

    added bonus

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var times=0;
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  times+=1
   res.send('respond with a resource');
 });
 
